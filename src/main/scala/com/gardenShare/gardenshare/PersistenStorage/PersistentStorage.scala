c3f437fd875d657b24f1f208b868542a5787d435
sdrafahl <shanedrafahl@gmail.com>
2020-05-17 23:37:09 -0500

    Wip - started working the persistent storage mechanism. Based off of the Mongo client interface. Will return to this in a week or so.

--- src/main/scala/com/gardenShare/gardenshare/PersistenStorage/PersistentStorage.scala
+++ src/main/scala/com/gardenShare/gardenshare/PersistenStorage/PersistentStorage.scala
@@ -1,5 +1,60 @@
 package com.gardenShare.gardenshare.Persistentstorage
 
-abstract class Persistenstorage[F[_]] {
-  def storeStuff = ???
+import com.gardenShare.gardenshare.Config.Config
+import cats.effect.IO
+import scala.jdk.CollectionConverters._
+import com.mongodb.MongoClientSettings
+import com.mongodb.ServerAddress
+import org.mongodb.scala.MongoClient
+import com.gardenShare.gardenshare.Config.MongoSettings
+import scala.language.implicitConversions
+import cats.effect.Async
+import org.mongodb.scala.Document
+import org.bson.BsonDocument
+import org.bson.BsonValue
+import org.bson.BsonElement
+
+case class DocumentStoreLocation(databaseName: String, collectionName: String)
+case class DocumentData(index: (String, BsonValue), values:Map[String, BsonValue])
+
+object DocumentData {
+  abstract class DocumentDataToMongoDocument {
+    def toDocument(doc:DocumentData): Document
+  }
+  implicit object DocumentDataToMongoDocumentInstance extends DocumentDataToMongoDocument {
+    def toDocument(doc:DocumentData): Document = Document.fromSeq((doc.values ++ Map(doc.index)).toSeq)
+  }
+  implicit class DocumentDataToMongoDocumentInstanceTypeClass(documentData:DocumentData) {
+    def toMongoDocument(implicit documentDataToMongoDocument :DocumentDataToMongoDocument) = documentDataToMongoDocument.toDocument(documentData)
+  }
 }
+
+abstract class PersistenStorage[F[_]] {
+  def store(document: DocumentData ,documentStoreLocation: DocumentStoreLocation)(implicit driver: DocumentStoreDriver[F]): F[Unit]
+}
+
+object PersistenStorage {
+  def apply[F[_]: PersistenStorage] = implicitly[PersistenStorage[F]]
+
+  implicit object IOPersistenStorage extends PersistenStorage[IO] {
+    def store(document: DocumentData ,documentStoreLocation: DocumentStoreLocation)(implicit driver: DocumentStoreDriver[IO]): IO[Unit] = ???
+  }
+}
+
+abstract class DocumentStoreDriver[F[_]] {
+  def insertOneDocument(document: DocumentData, documentStoreLocation: DocumentStoreLocation): F[Unit]
+}
+
+object DocumentStoreDriver {
+  implicit def apply[F[_]: Async](mongoSettings: MongoSettings) = new DocumentStoreDriver[F] {
+    val mongoConnectionString = s"mongodb://${mongoSettings.host}:${mongoSettings.password}@${mongoSettings.host}:${mongoSettings.port}"
+    val client: MongoClient = MongoClient(mongoConnectionString)
+
+    def insertOneDocument(document: DocumentData, documentStoreLocation: DocumentStoreLocation): F[Unit] = {
+      Async[F].async { cb =>
+        cb(Right(client.getDatabase(documentStoreLocation.databaseName).getCollection(documentStoreLocation.collectionName).insertOne(document.toMongoDocument)))
+      }
+    }
+  }
+}
+
--- src/test/scala/com/gardenShare/gardenshare/Config/ConfigSpec.scala
+++ src/test/scala/com/gardenShare/gardenshare/Config/ConfigSpec.scala
@@ -1,25 +1,32 @@
 package com.gardenShare.gardenshare.Config
 
 import utest._
-import org.mockito.Mockito
-import org.mockito.Matchers
-import org.mockito.MockitoSugar
-import org.mockito.ArgumentMatchersSugar
-import org.mockito.Mock
 
-object ConfigSpec extends TestSuite with MockitoSugar with ArgumentMatchersSugar {
+object ConfigSpec extends TestSuite {
   val tests = Tests{
-    //implicit val mockConfigDrivers = mock[ConfigDriver]("mockConfigDrivers")
     test("Config"){
       test("getMongoSettings") {
         test("Should return Mongo settings") {
+
           val testMongoHost = "www.mongo.com"
-          val testMongoHostKey = "mongoHostKey"
-          val expectedMongoConfig = MongoSettings(testMongoHost)
+          val testMongoPort = 1234
+          val testMongoPassword = "secret"
+          val testMongoUsername = "testUsername"
+          val expectedMongoConfig = MongoSettings(
+            testMongoHost,
+            testMongoPort,
+            testMongoPassword,
+            testMongoUsername
+          )
 
           implicit val mockConfigDriver = new ConfigDriver {
             def getValueString(key:String) = key match {
-              case testMongoHostKey => testMongoHost
+              case "MongoHost" => testMongoHost
+              case "MongoPassword" => testMongoPassword
+              case "MongoUsername" => testMongoPassword
+            }
+            def getValueInt(key:String) = key match {
+              case "MongoPort" => testMongoPort
             }
           }
 
