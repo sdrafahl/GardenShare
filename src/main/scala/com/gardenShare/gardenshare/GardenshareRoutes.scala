86cc87456af892bb279edf893fd0995d151e38ee
sdrafahl <shanedrafahl@gmail.com>
2020-09-04 21:30:29 -0500

    Changing the status code.

--- src/main/scala/com/gardenShare/gardenshare/GardenshareRoutes.scala
+++ src/main/scala/com/gardenShare/gardenshare/GardenshareRoutes.scala
@@ -95,8 +95,7 @@ object GardenshareRoutes {
         for {
           result <- processRequest
           newResp <- result match {
-            case Left(err) => BadRequest(ResponseBody(s"User Request Failed: ${err.getMessage()}").asJson.toString())
-              .map(a => a.copy(headers = a.headers.put(Header.apply("Content-Type", "application/json"))))
+            case Left(err) =>  Conflict(ResponseBody(s"User Request Failed: ${err.getMessage()}").asJson.toString())
             case Right(resp) => Ok(ResponseBody(s"User Request Made: ${resp.codeDeliveryDetails().toString()}").asJson.toString())
               .map(a => a.copy(headers = a.headers.put(Header.apply("Content-Type", "application/json"))))
           }
