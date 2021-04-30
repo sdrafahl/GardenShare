bb1887749b2bc1b2b608c8df9455290bff0391dc
sdrafahl <shanedrafahl@gmail.com>
2021-04-29 20:31:02 -0500

    Adding DB connection to resource.

--- src/main/scala/com/gardenShare/gardenshare/Main/Main.scala
+++ src/main/scala/com/gardenShare/gardenshare/Main/Main.scala
@@ -1,6 +1,6 @@
 package com.gardenShare.gardenshare
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Resource}
 import com.gardenShare.gardenshare.PostGresSetup
 import com.gardenShare.gardenshare.GetRoutesForEnv._
 import com.gardenShare.gardenshare.GetRoutes._
@@ -9,18 +9,21 @@ import io.circe.generic.auto._
 import com.typesafe.config.ConfigFactory
 import com.gardenShare.gardenshare.GetTypeSafeConfig
 import ParsingDecodingImplicits._
+import slick.jdbc.PostgresProfile
 
 object Main extends IOApp {
   val confPgm: IO[GetTypeSafeConfig[IO]] = IO(ConfigFactory.load()).map(conf => GetTypeSafeConfig.ioGetTypeSafeConfig(conf))
-
+  
   def run(args: List[String]) = for {    
-    typeSafeConfig <- confPgm    
-    x <- {
+    typeSafeConfig <- confPgm
+    x <- {     
       implicit val c = typeSafeConfig
       val executionValues = ConcurrencyHelper.createConcurrencyValues(4)
-      implicit val ec = executionValues._2
-      implicit val postgresClient = PostGresSetup.createPostgresClient
-      GardenshareServer.stream[IO].as(ExitCode.Success)
+      implicit val ec = executionValues._2      
+      Resource.make(IO(PostGresSetup.createPostgresClient))((a: PostgresProfile.backend.DatabaseDef) => IO(a.close())).use{dbClient =>
+        implicit val x = dbClient
+        GardenshareServer.stream[IO].as(ExitCode.Success)
+      }      
     }
   } yield x
 }
